db95e947ce9466282182d1efe8bfc75c6f44fbab
NolanRussell416 <m275544@usna.edu>
2024-10-22 09:46:04 -0400

    made a background

--- game.py
+++ game.py
@@ -2,19 +2,35 @@ import pygame
 
 # pygame setup
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
 
+#make a background
+WIDTH = 1280
+HEIGHT = 720
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+background = pygame.Surface((WIDTH,HEIGHT))
+light_green = (47,153,67)
+dark_green = (47,173,67)
+background.fill(dark_green)
+stripe_width = 30
+stripe = pygame.Surface((stripe_width, HEIGHT))
+stripe.fill(light_green)
+for x in range(0,WIDTH,stripe_width*2):
+    background.blit(stripe,(x,0))
+
+#start game
 while running:
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    #blit background to screen
+    screen.blit(background,(0,0))
 
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
+    
 
     # RENDER YOUR GAME HERE
 
