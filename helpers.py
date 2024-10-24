d09a308e2752a6fa58101219246b7b46b81467f6
NolanRussell416 <m275544@usna.edu>
2024-10-24 13:41:27 -0400

    continuing background

--- helpers.py
+++ helpers.py
@@ -13,13 +13,19 @@ def build_background(WIDTH, HEIGHT):
 
     #add layers
     horizontal = pygame.image.load('kenney_sports-pack/PNG/Elements/Element (2).png')
-    TILE_SIZE = horizontal.get_width()
+    corner1 = pygame.image.load('kenney_sports-pack/PNG/Elements/Element (5).png')
+    vertical = pygame.image.load('kenney_sports-pack/PNG/Elements/Element (1).png')
+    TILE_SIZE = vertical.get_height()
+    
+    # loop over x direction
+    for x in range(90,1140,TILE_SIZE):
+            # blit the tile to our BG
+            background.blit(horizontal, (x,10))
 
-    # # loop over x direction
-    # for x in range(0,WIDTH,TILE_SIZE):
-    #     # loop over y direction
-    #     for y in range(0,700, TILE_SIZE):
-    #         # blit the tile to our BG
-    #         background.blit(horizontal, (x,y))
+    background.blit(corner1, (30,10))
+
+    for y in range(60,592,TILE_SIZE):
+            # blit the tile to our BG
+            background.blit(vertical, (30,y))
         
     return background
